b9b7ecab37f0c374cf1ccc78e39b430a3ef5fb01
Michał Szymański <voiceofthunder0108@gmail.com>
2023-05-19 23:50:45 +0200

    Add class active to the correct article

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     /* [DONE] remove class 'active' from all article links  */
@@ -17,7 +18,7 @@ const titleClickHandler = function(event){
 
     /* [IN PROGRESS] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
-    
+
     clickedElement.classList.add('active');
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
@@ -26,10 +27,15 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+    
+    console.log(targetArticle);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
