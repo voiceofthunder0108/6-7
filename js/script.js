d1b1238d41f5c288034f36e2b1429c879791782d
Michał Szymański <voiceofthunder0108@gmail.com>
2023-05-19 19:02:29 +0200

    Add class active to link after click

--- js/script.js
+++ js/script.js
@@ -6,21 +6,24 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event)
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    
+    clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active')
+      activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
   
