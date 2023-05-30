ae191444ca2cb6651d9919dc6b61da13f1b09042
Michał Szymański <voiceofthunder0108@gmail.com>
2023-05-30 23:50:37 +0200

    Change Array into object and add tags counter

--- js/script.js
+++ js/script.js
@@ -71,8 +71,8 @@
 
   //Generowanie tagów w artykułach:
   const generateTags = function(){
-    /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
     /* START LOOP: for every article: */
@@ -92,9 +92,11 @@
         /* add generated code to html variable */
         html = html + tagHTML;
         /* [NEW] check if this link is NOT already in allTags */
-        if(allTags.indexOf(tagHTML) == -1){
-          /* [NEW] add generated code to allTags array */
-          allTags.push(tagHTML);
+        if(!allTags[tag]){
+          /* [NEW] add tag to allTags object */
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
         }
       /* END LOOP: for each tag */
       }
@@ -105,8 +107,18 @@
     /* [NEW] find list of tags in right column */
     const tagList = document.querySelector(optTagsListSelector);
 
-    /* [NEW] add html from allTags to tagList */
-    tagList.innerHTML = allTags.join(' ');
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' </a><span>(' + allTags[tag] + ')</span></li> ';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
   };
   
   generateTags();
